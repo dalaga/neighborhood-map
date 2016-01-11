a3452cb166354300ccb9abb0af518d43366ddbfd
Javier Salazar <jav_salazar@yahoo.com>
2016-01-11 12:11:34 -0800

    initial commit

---
+++ _src/app/js/app.js
@@ -0,0 +1,33 @@
+/* app.js
+ * This file provides the detais for the map application.
+ *
+ * TODO: more description
+ *
+ *
+ */
+
+
+/**
+ * Keep variables off global space
+ *
+ */
+(function ($, app, window) {
+
+
+	var model = {
+
+	};
+
+
+	var viewModel = {
+
+		// keep reference to viewModel even when context changes
+		var self = this;
+
+	};
+
+
+	ko.applyBindings(new ViewModel());
+
+
+})(jQuery, window.app || (window.app = {}), window);
